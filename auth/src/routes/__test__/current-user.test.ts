850fafb267f4c504a7826a5ca5374e71279df7b9
Adif <adif.ladiva@gmail.com>
2021-01-02 19:30:43 +0700

    Check github actions if test failed

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -8,9 +8,8 @@ it('responds with detail of current user', async () => {
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send({})
-    .expect(200);
+    .expect(400);
 
-  // console.log(response.body);
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
 
