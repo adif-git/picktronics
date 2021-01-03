2a0bde2bb4041a4ef0707fa70accfd84b3cf8e3d
Adif <adif.ladiva@gmail.com>
2021-01-03 12:02:21 +0700

    Revert back test payments

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -76,7 +76,7 @@ it('return a 201 with valid input', async () => {
       token: 'tok_visa',
       orderId: order.id,
     })
-    .expect(404);
+    .expect(201);
 
   const stripeCharges = await stripe.charges.list({ limit: 50 });
   const stripeCharge = stripeCharges.data.find((charge) => {
